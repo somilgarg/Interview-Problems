ce28976b77c85a2aab4538156444b3680c58ce0c
somilgarg181 <somilgarg181@gmail.com>
2019-08-03 13:45:12 +0530

    main method added

---
+++ JumpClouds.java
@@ -0,0 +1,14 @@
+/**
+ * Hackerran Interview Practice Problem
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+ * By: Somil Garg 
+ * Date:- 03/08/2019 
+ */ 
+
+
+public class JumpClouds{
+    public static void main(String args[]){
+            System.out.println("Jump clouds");
+    }
+
+}
